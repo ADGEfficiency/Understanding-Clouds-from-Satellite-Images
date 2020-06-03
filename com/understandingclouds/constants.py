1e985b8f5c0f0cd3c450ed74c72b2d17a928424b
Matan <matanrein@gmail.com>
2020-06-03 10:26:35 +0200

    fixed train images folder path

--- com/understandingclouds/constants.py
+++ com/understandingclouds/constants.py
@@ -1,12 +1,12 @@
 import argparse
 import sys
 
-test_locally = True
+#test_locally = True
 
-if test_locally:
-    DATA_DIR = '/home/matan/UnderstandingClouds/competition-data/'
-else:
-    DATA_DIR = ''
+#if test_locally:
+#    DATA_DIR = '/home/matan/UnderstandingClouds/competition-data/'
+#else:
+DATA_DIR = ''
 LABELS = ['Fish', 'Flower', 'Gravel', 'Sugar']
-TRAIN_IMAGES_FOLDER = DATA_DIR + 'train_images_320_480/'
+TRAIN_IMAGES_FOLDER = DATA_DIR + 'train_images/'
 TRAIN_DF_FILE = DATA_DIR + 'augmented_train_df.csv'
